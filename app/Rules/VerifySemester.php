e9c671cd652631f0113c586abee632c4ac1204c9
Andrew Weber <apweber@ksu.edu>
2018-10-10 14:29:28 -0500

    Added semester rules

---
+++ app/Rules/VerifySemester.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Rules;
+use App\Models\Course;
+use App\Models\Plan;
+use App\Models\Semester;
+use App\Models\Student;
+use spec\PhpSpec\Process\Prerequisites\SuitePrerequisitesSpec;
+
+
+Private Plan $plan;
+Private Student $student;
+
+
+
+//Checks to see if the student has the correct number of hours to be a full time student
+//Hours need to be less than 21 and greater than 0
+    public function CheckHours(Semester $semester){
+
+
+        $CreditHours = App\Models\Planrequirements:: where('semester_id' $semester->id)->sum('credits')->get();
+
+        if ($CreditHours > 21 || $CreditHours < 1){
+
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+
+
+//Checks to see if the student has the correct prereqs to take the current semester worth of classes
+    public function CheckPreReqs(Semester $semester){
+
+        %course = App\Models\Planrequirements:: where('semester_id' $semester->id)->get();
+
+        $StudentCompletedCourses = $student->completedcourses();
+        $prerequisites = Course::prerequisites();
+
+        foreach('course' $courses in %course->Course){
+            if (){
+
+                return false;
+            }
+            else{
+                return true;
+            }
+
+        }
+
+
+    }
+
+
+?>
