10d1ca5c1d0d82920d4425375130f777f127cf21
Andrew Weber <apweber@ksu.edu>
2018-10-15 14:24:48 -0500

    Finished the checking of prereqs

--- app/Rules/VerifySemester.php
+++ app/Rules/VerifySemester.php
@@ -15,8 +15,7 @@ class VerifySemester
 //Hours need to be less than 21 and greater than 0
     public function CheckHours(Semester $semester)
     {
-
-
+        //Gets the semester information and adds all the credit hours into credithours
         $CreditHours = App\Models\Planrequirements:: where('semester_id', $semester->id)->sum('credits')->get();
 
         if ($CreditHours > 21 || $CreditHours < 1) {
@@ -28,27 +27,29 @@ class VerifySemester
 
     }
 
-
 //Checks to see if the student has the correct prereqs to take the current semester worth of classes
     public function CheckPreReqs(Semester $semester, Student $student)
     {
 
-
         $courses = App\Models\Planrequirements:: where('semester_id', $semester->id)->get();
 
-        $StudentCompletedCourses = $student->completedcourses();
-        $prerequisites = Course::prerequisites();
+        $StudentCompletedCourses = $student->completedcourses;
 
         foreach ($courses as $course){
-        if () {
-
-            return false;
-        } else {
-            return true;
+            $prerequisites = $course->prerequisites;
+            if ($prerequisites == NULL){
+                return true;
+            }
+            foreach($prerequisites as $prereq) {
+                $coursenumberlookup = App\Models\Course :: where('id', $prereq->prerequisite_for_course_id);
+                if ($StudentCompletedCourses.contains('coursenumber' , $coursenumberlookup->number) ) {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
         }
-
-    }
-
 
     }
 
