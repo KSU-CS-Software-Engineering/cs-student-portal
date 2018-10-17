a19c2ba77488879d0ce6598d82cd0bc1bc666b5f
Ian Riley <ianriley97@ksu.edu>
2018-10-17 11:33:04 -0700

    Further work on the new tests.

--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -27,4 +27,13 @@ class RulesTest extends TestCase {
         //Make sure to grab the first one in the array (it'sthe only one in the array)
         $this->AssertEmpty($rules->CheckCISRequirementsPlan($plan[0]));
     }
+
+
+    public function testGraduationValidityDegreeRequirements() {
+        //Again we'll just grab exisitng data for the tests.
+        //Here we'll grab Plan4, because it matches the DegreeRequirements exactly.
+        $plan = App\Models\Plan::where('id', 4)->get();
+        //Need to fake the completed classes based on the course name of the DegreeRequirements
+        //From there, I can use the electivelist_id to choose a random course that matches the elective Id as the class we want.
+    }
 }
